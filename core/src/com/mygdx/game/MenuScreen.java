b9b6e325a1e063279b7a5336f084bcb0b6c4c4bb
Alex Cruz <alexcruz182@gmail.com>
2021-01-14 22:18:53 -0500

    more stuff

--- core/src/com/mygdx/game/MenuScreen.java
+++ core/src/com/mygdx/game/MenuScreen.java
@@ -1,13 +1,23 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
 
 import java.awt.*;
 
 public class MenuScreen implements Screen {
     private MyGame myGame;
+    Stage stage;
     public MenuScreen(MyGame myGame){
         this.myGame = myGame;
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.draw();
     }
 
     public MenuScreen(){
